5efd2e80539c737c46a77abe51d6dbaec91d3da7
hamzamalik0123 <hamzamalik0123@gmail.com>
2017-06-06 04:15:53 +0100

    database error

--- database/migrations/2017_05_25_111553_add_user_column_in_sites.php
+++ database/migrations/2017_05_25_111553_add_user_column_in_sites.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddUserColumnInCards extends Migration
+class AddUserColumnInSites extends Migration
 {
     /**
      * Run the migrations.
